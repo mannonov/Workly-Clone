3b7d466e79f21ba4ddcbd6ca69ecc8d67556dc6a
mannonov <jahongir.sh.mannonov@gmail.com>
2021-07-03 19:03:50 +0500

    mymodule added

--- app/src/main/java/com/jaxadev/worklyclone/MainActivity.kt
+++ app/src/main/java/com/jaxadev/worklyclone/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.jaxadev.worklyclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.jaxadev.mylibrary.PatternLockView
+import com.jaxadev.mylibrary.listener.PatternLockViewListener
+import com.jaxadev.mylibrary.utils.PatternLockUtils
+import com.jaxadev.worklyclone.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), PatternLockViewListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.patternLockView.addPatternLockListener(this)
+
+
     }
+
+    override fun onStarted() {
+
+    }
+
+    override fun onProgress(progressPattern: MutableList<PatternLockView.Dot>?) {
+
+    }
+
+    override fun onComplete(pattern: MutableList<PatternLockView.Dot>?) {
+        if (PatternLockUtils.patternToString(binding.patternLockView, pattern).equals("0125")) {
+            binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT)
+            Toast.makeText(applicationContext, "Pattern is correct", Toast.LENGTH_SHORT).show()
+        } else {
+            binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG)
+            Toast.makeText(applicationContext, "Pattern is correct", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCleared() {
+
+    }
+
 }
